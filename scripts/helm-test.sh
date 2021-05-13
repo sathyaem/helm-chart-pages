7a60a21d74c83ebece03b11a9b699738b3c2e57f
your-team-name <your-git-email-address>
2021-05-13 14:35:31 +0100

    Adding TestSuite 1.28

--- scripts/helm-test.sh
+++ scripts/helm-test.sh
@@ -8,17 +8,16 @@ RELEASE_NAME="$2"
 
 kubectl config get-contexts
 kubectl create ns "$NAMESPACE"
-kubectl config set-context --current --namespace "$NAMESPACE" 
-kubectl config get-contexts     
+kubectl config set-context --current --namespace "$NAMESPACE"      
 helm lint pages 
-# helm template pages
+helm template pages
 
 
-echo "------------------------Start time was is--------  $(date +%Y-%m-%dT%H%M%S%z)"
+echo "------------------------Start time is--------  $(date +%Y-%m-%dT%H%M%S%z)"
 
 helm upgrade --install "$RELEASE_NAME" pages --debug
 
-echo "------------------------End time was is--------  $(date +%Y-%m-%dT%H%M%S%z)"
+echo "------------------------End time is--------  $(date +%Y-%m-%dT%H%M%S%z)"
 
 # helm upgrade --install "$RELEASE_NAME" pages --create-namespace --debug
 echo '---------------------Started testing--------------'
