e5b6fc96835e69cd650fedf344b03b95dbd720d8
your-team-name <your-git-email-address>
2021-05-18 07:07:22 +0100

    Adding TestSuite 1.35

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,8 @@ RELEASE_NAME="$2"
 kubectl get ns "$NAMESPACE" &> /dev/null || kubectl create ns "$NAMESPACE"
 
 helm upgrade --install "$RELEASE_NAME" pages -n "$NAMESPACE" --debug
-
+helm list
+kubectl config current-context
 sleep 30s
 
 kubectl get pods -n "$NAMESPACE"
